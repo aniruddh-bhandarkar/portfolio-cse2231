34c8cb2cdc25308576f9e732bff7dc506ceb2912
Aniruddh Bhandarkar <apb92710@gmail.com>
2024-11-21 11:39:57 -0500

    Revert "Implemented  changes to the interfaces and layering"
    This reverts commit 1eec7d6ab2f43894ff6c6133887b87e77b7586dd.

--- src/Airplane.java
+++ src/Airplane.java
@@ -3,6 +3,55 @@
  */
 public interface Airplane extends AirplaneKernel {
 
-    String displayDefinedMetrics();
+    /**
+     * Sets the altitude of the airplane.
+     *
+     * @param altitude
+     *            The new altitude value.
+     */
+    void setAltitude(double altitude);
 
+    /**
+     * Gets the current altitude of the airplane.
+     *
+     * @return The current altitude.
+     */
+    double getAltitude();
+
+    /**
+     * Sets the speed of the airplane.
+     *
+     * @param speed
+     *            The new speed value.
+     */
+    void setSpeed(double speed);
+
+    /**
+     * Gets the current speed of the airplane.
+     *
+     * @return The current speed.
+     */
+    double getSpeed();
+
+    /**
+     * Sets the direction of the airplane.
+     *
+     * @param direction
+     *            The new direction in degrees.
+     */
+    void setDirection(int direction);
+
+    /**
+     * Gets the current direction of the airplane.
+     *
+     * @return The current direction.
+     */
+    int getDirection();
+
+    /**
+     * Gets the fuel quantity of the airplane.
+     *
+     * @return The current fuel quantity.
+     */
+    double getFuelQuantity();
 }
--- src/AirplaneSecondary.java
+++ src/AirplaneSecondary.java
@@ -1,18 +1,37 @@
 /**
- * The secondary methods for AirplaneKernel, add the method bodies to finish the
- * abstract class assignment.
+ * The secondary methods for AirplaneKernel.
  */
-public abstract class AirplaneSecondary implements Airplane {
+public interface AirplaneSecondary {
 
-    @Override
-    public final String displayDefinedMetrics() {
-        String output = "";
-        for (Mode mode : Mode.values()) {
-            if (this.isDefined(mode)) {
-                output += this.getMetric(mode);
-            }
-        }
-        return output;
-    }
+    /**
+     * Adjusts the airplane's altitude incrementally.
+     *
+     * @param increment
+     *            The altitude increment value (can be positive or negative).
+     */
+    void adjustAltitude(double increment);
 
+    /**
+     * Adjusts the airplane's speed incrementally.
+     *
+     * @param increment
+     *            The speed increment value (can be positive or negative).
+     */
+    void adjustSpeed(double increment);
+
+    /**
+     * Adjusts the airplane's direction incrementally.
+     *
+     * @param increment
+     *            The direction increment value (in degrees, can be positive or
+     *            negative).
+     */
+    void adjustDirection(int increment);
+
+    /**
+     * Calculates the remaining flight time based on current speed and fuel.
+     *
+     * @return Estimated flight time in hours.
+     */
+    double calculateRemainingFlightTime();
 }
